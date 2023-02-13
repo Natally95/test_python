02a19b7bb32f76e131d3a37d8b9778abe3112511
Наталья <natali.mamtseva@yandex.ru>
2023-02-13 22:58:10 +0300

    new2

--- main2.py
+++ main2.py
@@ -1,13 +1,1 @@
-from random import randint
-name = []
-for i in list(range(15)):
-    name.append(randint(-6,50))
-print("первоначальная последовательность:", name)
-sum = 0
-kolvo = 0
-sra = 0
-for i in name:
-    if i % 2 == 0:
-        index = name.index(i)
-        name[index] = 'вот он!'
-print('нашёлся кратный 2: ', name)
+print("fjhdgfhjdflg")
